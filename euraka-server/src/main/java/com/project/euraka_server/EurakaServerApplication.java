3312bafa654cdf554a9a40a653c0ddcad2fedcd3
aevorix <aevorix7@gmail.com>
2025-06-15 23:52:54 +0530

    Add files via upload

---
+++ euraka-server/src/main/java/com/project/euraka_server/EurakaServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.euraka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurakaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurakaServerApplication.class, args);
+	}
+
+}
